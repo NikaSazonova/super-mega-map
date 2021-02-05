fc0bc6570abc82f42551f54047fdbd7415207d24
NikaSazonova <74972433+NikaSazonova@users.noreply.github.com>
2021-02-05 17:02:10 +0300

    Update main.py
    do something new

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 SCREEN_SIZE = [600, 450]
 
 
-class Example(QWidget):
+l1 = 37.530887
+l2 = 55.703118
+
+spn1 = 0.01
+spn2 = 0.01
+
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={l1},{l2}&spn={spn1},{spn2}&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -31,13 +37,15 @@ class Example(QWidget):
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
+        self.setWindowTitle('Отображение карты 1.0')
 
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
+        self.refresh_image()
+
+    def refresh_image(self):
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
+        self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
@@ -47,6 +55,6 @@ class Example(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MainWindow()
     ex.show()
     sys.exit(app.exec())
